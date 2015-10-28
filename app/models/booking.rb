35dea9de5747cae0539ccdeea239a237ef0ca4d2
artyommedvedev <artyom.s.medvedev@gmail.com>
2015-10-28 14:24:32 +0200

    added correct date check for next year

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -24,15 +24,63 @@ class Booking < ActiveRecord::Base
     all_rooms = Room.all
     room_numbers = all_rooms.pluck(:number)
     all_bookings = Booking.all
-    all_bookings.each do |b|
-      if ((b[:begining].yday...b[:ending].yday) === d_st.yday) ||
-          ((b[:begining].yday...b[:ending].yday) === d_end.yday)
-        if room_numbers.include? (b[:number])
-          room_numbers.delete(b[:number])
-        end
-      end
+
+    year_now = Date.today.year
+    if d_st.year > year_now
+      d_st_mod = d_st.yday + 365
+    else
+      d_st_mod = d_st.yday
     end
-    #in array room_numbers left only available for booking for selected date
+
+    if d_end.year > year_now
+      d_end_mod = d_end.yday + 365
+    else
+      d_end_mod = d_end.yday
+    end
+
+    all_bookings.each do |b|
+
+        if b[:begining].year > year_now
+          b_st_mod = b[:begining].yday + 365
+        else
+          b_st_mod = b[:begining].yday
+        end
+        if b[:ending].year > year_now
+          b_end_mod = b[:ending].yday + 365
+        else
+          b_end_mod = b[:ending].yday
+        end
+
+      #------------------
+        if ((b_st_mod...b_end_mod) === d_st_mod) ||
+            ((b_st_mod...b_end_mod) === d_end_mod)
+          if room_numbers.include? (b[:number])
+            room_numbers.delete(b[:number])
+          end
+        end
+
+        if (b_st_mod >= d_st_mod) && (b_end_mod <= d_end_mod)
+          if room_numbers.include? (b[:number])
+            room_numbers.delete(b[:number])
+          end
+        end
+
+=begin
+        if ((b[:begining].yday...b[:ending].yday) === d_st.yday) ||
+            ((b[:begining].yday...b[:ending].yday) === d_end.yday)
+          if room_numbers.include? (b[:number])
+            room_numbers.delete(b[:number])
+          end
+        end
+        #
+        if (b[:begining].yday >= d_st.yday) && (b[:ending].yday <= d_end.yday)
+          if room_numbers.include? (b[:number])
+            room_numbers.delete(b[:number])
+          end
+        end
+=end
+      end
+     #in array room_numbers left only available for booking for selected date
     return room_numbers
   end
 end
